f12e11e4a2e2327b1b03fb7cf10f77ca490d86b3
Lindsay Culver & Rachael Berecka <student@epicodus.com>
2015-08-25 10:46:37 -0700

    Add the upcase method to the names of surveys.

--- spec/question_spec.rb
+++ spec/question_spec.rb
@@ -3,7 +3,7 @@ require('spec_helper')
 describe(Question) do
   describe("#survey") do
     it('says which survey the question belongs to') do
-      survey = Survey.create({:name => "Home for the holidays"})
+      survey = Survey.create({:name => "HOME FOR THE HOLIDAYS"})
       question = Question.create({:content => "Why me?", :survey_id => survey.id() })
       expect(question.survey()).to(eq(survey))
     end
--- spec/survey_spec.rb
+++ spec/survey_spec.rb
@@ -4,10 +4,15 @@ describe(Survey) do
 
   describe("#questions") do
     it("give the questions in the survey") do
-      test_survey = Survey.create({:name => "Home for the holidays"})
+      test_survey = Survey.create({:name => "HOME FOR THE HOLIDAYS"})
       test_question1 = Question.create({:content => "Why me?", :survey_id => test_survey.id})
       test_question2 = Question.create({:content => "Why no babies?", :survey_id => test_survey.id})
       expect(test_survey.questions()).to(eq([test_question1, test_question2]))
     end
+  end
+
+  it("converts the name to upcase") do
+    test_survey = Survey.create({:name => "beard or no beard?"})
+    expect(test_survey.name()).to(eq("BEARD OR NO BEARD?"))
   end
 end
